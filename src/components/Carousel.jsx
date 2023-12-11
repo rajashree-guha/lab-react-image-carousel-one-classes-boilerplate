92edc100e48418e23b49d82b664b8dedf6c89e70
rajashree-guha <rajashree.625.guha@gmail.com>
2023-12-11 17:05:07 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,62 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+  constructor(){
+    super()
+
+    //set an initial state
+    this.state={
+      currIndex:0
+    }
+  } 
+
+increaseCount=()=>{
+    if(this.state.currIndex>=images.length-1){
+        this.setState({
+         currIndex:0
+    })
+    }
+    else{
+    this.setState({
+        currIndex: this.state.currIndex + 1
+    })
+  }
+}
+
+  decreaseCount=()=>{
+    if(this.state.currIndex==0){
+        this.setState({
+         currIndex:2
+    })
+    }
+    else{
+    this.setState({
+        currIndex: this.state.currIndex - 1
+    })
+  }
+  }
+
+  componentDidUpdate(){
+    console.log(this.state)
+  }
+
+  render(){
+    return(
+      <div className="carousel-container flex">
+          <div className="leftArrow arrowDiv flex" onClick={this.decreaseCount}>
+            <ArrowBackIosIcon/>
+          </div>
+          <div className="flex">
+            <h2 className="title">{images[this.state.currIndex].title}</h2>
+            <img src={images[this.state.currIndex].img} alt="" />
+            <h4 className="caption">{images[this.state.currIndex].subtitle}</h4>
+          </div>
+          <div className="rightArrow arrowDiv flex" onClick={this.increaseCount}>
+            <ArrowForwardIosIcon/>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default Carousel;
